65e54a9d7a3cbfd7726a04d55abf454b68fef963
Joselin Grez <joselingrez.m@hotmail.es>
2017-06-19 16:30:50 -0300

    Ambos botones con funcionalidad

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
         var directionsDisplay = new google.maps.DirectionsRenderer;
         var map = new google.maps.Map(document.getElementById("map"), {
           zoom: 7,
-          center: {lat: 41.85, lng: -87.65}
+          center: {lat: -33.4569400, lng: -70.6482700}
         });
         directionsDisplay.setMap(map);
 
@@ -25,4 +25,35 @@
           }
         });
       }
+
+      function buscar(){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);     
     }
+  }
+  document.getElementById("encuentrame").addEventListener("click",buscar);
+  var latitud,longitud;
+
+
+/*Con funcionExito obtendremos nuestra latitud o longitud y ademas crearemos un marcador de nuestra ubicacion*/
+  var funcionExito = function(posicion) {
+    latitud = posicion.coords.latitude;
+    longitud = posicion.coords.longitude;
+
+    var miUbicacion = new google.maps.Marker({
+      position: {lat:latitud, lng:longitud},
+      animation: google.maps.Animation.DROP,
+      map: map
+    });
+
+  map.setZoom(17);
+  map.setCenter({lat:latitud, lng:longitud});
+}
+
+  var funcionError = function (error) {
+  alert("Tenemos un problema con encontrar tu ubicacion");
+  }
+}
+  
+
+

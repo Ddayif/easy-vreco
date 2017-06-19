a764f075381ccc237e46afff79395cb966253b4e
Joselin Grez <joselingrez.m@hotmail.es>
2017-06-19 16:06:13 -0300

    Agregando js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,72 @@
-function initMap(){
-	var map = new google.maps.Map(document.getElementById("map"),{
-		zoom: 5,
-		center: {lat: -9.1191427, lng: -77.0349046},
-		mapTypeControl:false,
-		zommControl:false,
-		streetViewControl:false
-	});
+/*function initMap() {
+  var myLatLng = document.getElementById("origen").value;
+  var destination = document.getElementById("destino").value;
 
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: {lat: -9.1191427, lng: -77.0349046},
+    mapTypeControl:false,
+    zommControl:false,
+    streetViewControl:false
+  });
 
-/*getCurrentPosition permite al usuario obtener su ubicacion actual, el parametro functionExito se
-ejecuta solo cuando el usuario comparte su ubicacion, miestras que funcionError se ejecuta cuando
-se produce un error en la geolocalizacion*/
-	function buscar(){
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(funcionExito, funcionError);			
-		}
-	}
-	document.getElementById("encuentrame").addEventListener("click",buscar);
-	var latitud,longitud;
-
-
-/*Con funcionExito obtendremos nuestra latitud o longitud y ademas crearemos un marcador de nuestra ubicacion*/
-	var funcionExito = function(posicion) {
-		latitud = posicion.coords.latitude;
-		longitud = posicion.coords.longitude;
-
-		var miUbicacion = new google.maps.Marker({
-			position: {lat:latitud, lng:longitud},
-			animation: google.maps.Animation.DROP,
-			map: map
-		});
-
-	map.setZoom(17);
-	map.setCenter({lat:latitud, lng:longitud});
+  function buscar(){
+  var service = new google.maps.DistanceMatrixService(funcionExito, callback);
+  service.getDistanceMatrix(
+  {
 }
 
-	var funcionError = function (error) {
-	alert("Tenemos un problema con encontrar tu ubicacion");
-	}
+      document.getElementById("encuentrame").addEventListener("click",buscar);
+        var myLatLng,destination;
+
+  
+    var funcionExito = function(posicion) {
+    position: {lat:myLatLng, lng:destination},
+    travelMode: 'DRIVING',
+    map: map,
+    avoidHighways: Boolean,
+    avoidTolls: Boolean,
+    title: 'Hello World!',
+  }, callback);
+    
+  function callback(response, status) {
+  // See Parsing the Results for
+  // the basics of a callback function.
+}
+}
+*/
+
+
+function initMap() {
+  var myLatLng = document.getElementById("origen").value;
+  var destination = document.getElementById("destino").value;
+
+  var map = new google.maps.Map(document.getElementById("map"), {
+  center: {lat: 55.53, lng: 9.4},
+  zoom: 5
+  });
+
+
+   document.getElementById("ruta").addEventListener("click", function() {
+          getDistanceMatrix(myLatLng, destination);
+        });
+      }
+
+
+
+var service = new google.maps.DistanceMatrixService();
+service.getDistanceMatrix(
+  {
+    origins: myLatLng,
+    destinations: destination,
+    travelMode: 'DRIVING',
+    drivingOptions: DrivingOptions,
+    avoidHighways: Boolean,
+    avoidTolls: Boolean,
+  }, callback);
+
+function callback(response, status) {
+  // See Parsing the Results for
+  // the basics of a callback function.
+}
 }
